da6ce228f3a45402b5e371244c33b98b5d6c76bb
NicoBrown <n.brown2017@outlook.com>
2022-11-28 20:25:03 +0000

    app rename

--- Kwik_split/urls.py
+++ Kwik_split/urls.py
@@ -1,4 +1,4 @@
-"""quik_split URL Configuration
+"""Kwik_split URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- Kwik_split/wsgi.py
+++ Kwik_split/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quik_split project.
+WSGI config for Kwik_split project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quik_split.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kwik_split.settings')
 
 application = get_wsgi_application()
