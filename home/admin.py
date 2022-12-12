0a218789b95d390c7ee1d0f23814cf64d63a2863
NicoBrown <n.brown2017@outlook.com>
2022-12-12 12:16:33 +0000

    feat:navbar started feat: profile model added style: css & js files added

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
+from .models import Profile
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 # Register your models here.
+
+# ...
+admin.site.register(Profile)
