f7326fa80c3aee3f3df45db477a3ccb410402be3
NicoBrown <n.brown2017@outlook.com>
2022-12-19 20:19:16 +0000

    feat: profile page set up

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,5 @@
+#from django_countries.fields import CountryField
+from django.dispatch import receiver
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
@@ -5,21 +7,28 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Profile(models.Model):
+class profiles(models.Model):
+    """
+    A user profile model for each user
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField(
-        "self", related_name="followed_by", symmetrical=False, blank=True
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
     )
 
     def __str__(self):
         return self.user.username
 
 
-def create_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
     if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-
-
-# Create a Profile for each new user.
-post_save.connect(create_profile, sender=User)
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
