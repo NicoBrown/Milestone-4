1b92b96c03ffb29c926df662dba06cb5981cf2d6
NicoBrown <n.brown2017@outlook.com>
2022-12-31 14:53:53 +0000

    fix: expenses models updated

--- expenses/models.py
+++ expenses/models.py
@@ -8,31 +8,21 @@ from django.db import models
 
 class Expense(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
-    title = models.CharField(max_length=254)
+    supplier_name = models.CharField(max_length=254, null=True)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
+    quantity = models.IntegerField(default=1, null=True, blank=True)
+    amount = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    unit_price = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     data = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
-class ExpenseLineItem(models.Model):
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    title = models.CharField(max_length=254)
-    description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
+    total_amount = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    data = models.DateField(auto_now=True)
+    total_tax_amount = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    net_amount = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
