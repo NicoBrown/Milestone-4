1ab68f377878e813feb3b4de4dbc09aea9bbda0d
NicoBrown <n.brown2017@outlook.com>
2023-01-30 18:14:06 +0000

    fix: credentials incorrect due to error

--- expenses/views.py
+++ expenses/views.py
@@ -28,7 +28,8 @@ location = os.environ["DOCUMENT_AI_LOCATION"]  # Format is 'us' or 'eu'
 # Create processor before running sample, Refer to https://cloud.google.com/document-ai/docs/manage-processor-versions for more information
 processor_id = os.environ["DOCUMENT_AI_PROCESSOR_ID"]
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"kwik-split-0a449986ee26.json"
+# add google credentials json file to project root for local testing
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"kwik-split-0a449986ee26.json"
 
 
 @login_required
