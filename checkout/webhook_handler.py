43cad09c050b7cb5c0ecffb25d166dd0aa64fa47
NicoBrown <n.brown2017@outlook.com>
2023-01-30 23:00:34 +0000

    fix: model reference removed

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .models import Checkout_transaction
 from expenses.models import Expense, OrderLineItem
 from profiles.models import UserProfile
 
